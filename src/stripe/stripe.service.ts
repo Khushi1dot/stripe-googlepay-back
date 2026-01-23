eaca509f7c7d3593d79793030ad8fa48d3a1fe75
Khushi_Kumari <khushi.kuhmari@dotsquares.com>
2026-01-23 11:02:09 +0530

    update

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,3 +1,42 @@
+// import { Injectable } from '@nestjs/common';
+// import Stripe from 'stripe';
+// import { ConfigService } from '@nestjs/config';
+
+// @Injectable()
+// export class StripeService {
+//   private stripe: Stripe;
+
+//   constructor(private configService: ConfigService) {
+//     this.stripe = new Stripe(
+//       this.configService.get<string>('STRIPE_SECRET_KEY')!,
+//       {
+//     // apiVersion: "2025-12-15.clover",
+//   },
+//     );
+//   }
+
+//   createPaymentIntent(amount: number) {
+//     return this.stripe.paymentIntents.create({
+//       amount,
+//       currency: 'usd',
+//       // payment_method_types: ['card', 'google_pay', 'apple_pay']
+//       automatic_payment_methods: {
+//         enabled: true,
+//       },
+      
+//     });
+//   }
+
+//   verifyWebhook(signature: string, body: Buffer) {
+//     return this.stripe.webhooks.constructEvent(
+//       body,
+//       signature,
+    
+//       this.configService.get<string>('STRIPE_WEBHOOK_SECRET')!,
+    
+//     );
+//   }
+// }
 import { Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { ConfigService } from '@nestjs/config';
@@ -9,9 +48,7 @@ export class StripeService {
   constructor(private configService: ConfigService) {
     this.stripe = new Stripe(
       this.configService.get<string>('STRIPE_SECRET_KEY')!,
-      {
-    // apiVersion: "2025-12-15.clover",
-  },
+     
     );
   }
 
@@ -19,11 +56,11 @@ export class StripeService {
     return this.stripe.paymentIntents.create({
       amount,
       currency: 'usd',
-      payment_method_types: ['card', 'google_pay', 'apple_pay']
-      // automatic_payment_methods: {
+      // automatic_payment_methods: {  // ← USE THIS with v20
       //   enabled: true,
+      //   allow_redirects: 'never', // Keeps only wallets + cards
       // },
-      
+      payment_method_types: ['card', 'google_pay', 'apple_pay'],
     });
   }
 
@@ -31,9 +68,7 @@ export class StripeService {
     return this.stripe.webhooks.constructEvent(
       body,
       signature,
-    
       this.configService.get<string>('STRIPE_WEBHOOK_SECRET')!,
-    
     );
   }
 }
